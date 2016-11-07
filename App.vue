6b5edc8e2c271e9fb61f6dc448f4931b5ff880f6
Frank van Wijk <fvanwijk@xebia.com>
2016-11-07 10:33:17 +0100

    Add app with example

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <label>Select option</label>
+    <multi-select
+      :value="selected"
+      :options="options"
+      :searchable="false"
+      @update="updateSelected"
+    ></multi-select>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import MultiSelect from 'vue-multiselect/lib/Multiselect.vue';
+Vue.component('multiSelect', MultiSelect);
+
+export default {
+  name: 'app',
+  components: { MultiSelect },
+  data () {
+    return {
+      selected: null,
+      options: ['list', 'of', 'options']
+    }
+  },
+  methods: {
+    updateSelected (newSelected) {
+      this.selected = newSelected
+    }
+  }
+}
+</script>
